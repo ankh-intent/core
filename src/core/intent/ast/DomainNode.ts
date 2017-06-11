41589965af69958168e82715aa9794936515d007
ankhzet <ankhzet@gmail.com>
2017-06-12 00:51:58 +0300

    Add Use as child node for domains

--- src/core/intent/ast/DomainNode.ts
+++ src/core/intent/ast/DomainNode.ts
@@ -1,8 +1,10 @@
 
 import { AbstractNode } from '../../tree/AbstractNode';
 import { TypeDefNode } from './TypeNode';
+import { UseNode } from './UseNode';
 
 export class DomainNode extends AbstractNode {
   public identifier: string;
   public types: {[name: string]: TypeDefNode};
+  public uses: {[name: string]: UseNode};
 }
--- src/core/intent/builder/DomainBuilder.ts
+++ src/core/intent/builder/DomainBuilder.ts
@@ -3,9 +3,11 @@ import { Tokens } from '../../parser/Tokens';
 import { DomainNode } from '../ast/DomainNode';
 import { TypeDefBuilder } from './TypeDefBuilder';
 import { BaseBuilder } from './BaseBuilder';
+import { UseBuilder } from './UseBuilder';
 
 export interface DomainChildren {
   typedef: TypeDefBuilder;
+  use: UseBuilder;
 }
 
 export class DomainBuilder extends BaseBuilder<DomainNode, DomainChildren> {
@@ -16,10 +18,23 @@ export class DomainBuilder extends BaseBuilder<DomainNode, DomainChildren> {
 
     let { value: identifier } = tokens.get({type: 'identifier'});
     let types = {};
+    let uses = {};
 
     tokens.ensure({value: '{'});
 
     while (true) {
+      let use = this.child.use.build(tokens);
+
+      if (use) {
+        if (uses[use.alias]) {
+          throw tokens.error(`Use with the same name "${use.alias}" already present`);
+        }
+
+        uses[use.alias] = use;
+
+        continue;
+      }
+
       let type = this.child.typedef.build(tokens);
 
       if (type) {
@@ -40,6 +55,7 @@ export class DomainBuilder extends BaseBuilder<DomainNode, DomainChildren> {
     let domain = new DomainNode();
     domain.identifier = identifier;
     domain.types = types;
+    domain.uses = uses;
 
     return domain;
   }
