d78d936b0a4928891a4f46bcf5532eec48fb69f9
Yegor Shovkun <ankhzet@gmail.com>
2017-06-12 14:21:54 +0300

    Refactoring: move stats logging into corresponding consumer

--- bin/intent.ts
+++ bin/intent.ts
@@ -6,22 +6,9 @@ if (process.env.ENV === 'development') {
 
 import config from './config';
 import { Core } from '../src/Core';
-import { StatEvent } from '../src/core/flow/events/StatEvent';
 
 ((core: Core) => {
-  let logger = {
-    log: (event, ...args) => {
-      console.log(`[INTENT/${event.type}]:`, ...args);
-    },
-    warn: (event, ...args) => {
-      console.warn(`[INTENT/WARN/${event.type}]:`, ...args);
-    },
-    error: (event, ...args) => {
-      console.error(`[INTENT/UNCAUGHT/${event.type}]:`, ...args);
-    },
-  };
-
-  let options = core.bootstrap({...config(core), ...{
+  core.bootstrap({...config(core), ...{
     watch: {
       aggregation: 0,
     },
@@ -31,16 +18,8 @@ import { StatEvent } from '../src/core/flow/events/StatEvent';
     let { type, data} = event;
 
     switch (type) {
-      case StatEvent.type():
-        if (data.stat === 'ready') {
-          if (options.watch) {
-            logger.log(event, 'Watching...');
-          }
-        }
-        break;
-
       default:
-        logger.log(event, JSON.stringify(data));
+        console.log(event, JSON.stringify(data));
     }
   });
 
--- src/core/flow/consumers/StatConsumer.ts
+++ src/core/flow/consumers/StatConsumer.ts
@@ -1,18 +1,103 @@
 
-import { CoreEvent } from '../CoreEvent';
+import { BaseCoreEvent, CoreEvent } from '../CoreEvent';
 import { AbstractConsumer } from '../AbstractConsumer';
 
 import { StatEvent } from '../events/StatEvent';
+import { CoreOptions } from '../../../Core';
+import { CoreEventBus } from '../CoreEventBus';
+import { Strings } from "../../../intent-utils/Strings";
+import { Source } from '../../source/Source';
 
 export class StatConsumer extends AbstractConsumer<StatEvent, any>{
+  private options: CoreOptions;
+  private logger;
+  private processors: {
+    ready: ReadyStat;
+    emitted: EmittedStat;
+  };
+
+  public constructor(bus: CoreEventBus, options: CoreOptions) {
+    super(bus);
+    this.options = options;
+    this.logger = new Logger();
+    this.processors = {
+      ready: new ReadyStat(this, this.logger, options),
+      emitted: new EmittedStat(this, this.logger, options),
+    };
+  }
 
   public supports(event: CoreEvent<any>): boolean {
     return event.type === StatEvent.type();
   }
 
   public process(event: StatEvent) {
-    if (event.data.stat === 'ready') {
-      return event;
+    let stat = event.data.stat;
+
+    if (this.processors[stat.type]) {
+      return this.processors[stat.type].process(event, stat);
     }
+  }
+}
+
+
+class BaseStat {
+  protected consumer: StatConsumer;
+  protected options: CoreOptions;
+  protected logger: Logger;
+
+  public constructor(consumer: StatConsumer, logger: Logger, options: CoreOptions) {
+    this.consumer = consumer;
+    this.logger = logger;
+    this.options = options;
+  }
+
+  public process(event: StatEvent, data: any) {
+  }
+}
+
+class ReadyStat extends BaseStat {
+  public process(event: StatEvent) {
+    if (this.options.watch) {
+      this.logger.log(event, 'Watching...');
+    }
+  }
+}
+
+class EmittedStat extends BaseStat {
+  public process(event: StatEvent, { chip, index, content, start, end }) {
+    let path = (<Source>content).reference;
+    let common = Strings.longestCommon([
+      path,
+      this.options.resolver.paths.project,
+      this.options.resolver.paths.intent
+    ]).pop();
+    let cause = chip.hane;
+
+    while (event) {
+      event = event.data.parent;
+
+      if (event) {
+
+      }
+    }
+
+    this.logger.log(
+      `${Strings.pad(String(index), 5, ' ', true)} ` +
+      `[${Strings.pad(cause, 10, ' ', true)}] ` +
+      `${Strings.pad(path.replace(new RegExp(`^${common}`), '@'), 60)} ` +
+      `~${(end - start)} ms`
+    );
+  }
+}
+
+class Logger {
+  log(event, ...args) {
+    console.log((event instanceof BaseCoreEvent) ? `[INTENT/${event.type}]:` : event, ...args);
+  }
+  warn(event, ...args) {
+    console.warn((event instanceof BaseCoreEvent) ? `[INTENT/WARN/${event.type}]:` : event, ...args);
+  }
+  error(event, ...args) {
+    console.error((event instanceof BaseCoreEvent) ? `[INTENT/UNCAUGHT/${event.type}]:` : event, ...args);
   }
 }
