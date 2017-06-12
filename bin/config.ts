0d0985fe3735cfdf674748c94a93a1265c84495b
Yegor Shovkun <ankhzet@gmail.com>
2017-06-12 16:43:17 +0300

    Fix: project default dir

--- bin/config.ts
+++ bin/config.ts
@@ -11,7 +11,7 @@ export default (core: Core) => {
     ],
     resolver: {
       paths: {
-        project: __dirname,
+        project: __dirname.replace('/build/', '/'),
       },
     },
     watch: {
