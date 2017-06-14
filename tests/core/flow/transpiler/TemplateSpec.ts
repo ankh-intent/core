69629c9c3df6571eba7647e51f75d2801909920f
ankhzet <ankhzet@gmail.com>
2017-06-14 22:44:42 +0300

    Fix: cleanup Template spec for now

--- tests/core/flow/transpiler/TemplateSpec.ts
+++ tests/core/flow/transpiler/TemplateSpec.ts
@@ -1,15 +1,6 @@
 
 import { AbstractTemplate } from '../../../../src/core/flow/transpiler/templates/AbstractTemplate';
 
-class MockTemplate<T> extends AbstractTemplate<T> {
-  public code: string;
-
-  public resolve(data: T, property: string) {
-    return data[property];
-  }
-
-}
-
 describe('Template', () => {
 
   describe('constructor()', () => {
@@ -23,14 +14,9 @@ describe('Template', () => {
   describe('compile()', () => {
 
     it('should compile empty template to no-op', () => {
-      let template = new MockTemplate(null, null);
-
-    })
+    });
 
     it('plaintext should be left as is', () => {
-      let template = new MockTemplate(null, null);
-      template.code = "";
-      template.com
     })
 
   });
