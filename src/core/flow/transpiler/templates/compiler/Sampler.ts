23e9e21a2067eea43c762203fd0e13a5108dc38e
ankhzet <ankhzet@gmail.com>
2017-06-15 08:46:57 +0300

    Update Sampler code and tests

--- src/core/flow/transpiler/templates/compiler/Sampler.ts
+++ src/core/flow/transpiler/templates/compiler/Sampler.ts
@@ -1,9 +1,11 @@
 
-import { MatchedPlaceholder } from './SamplerInterface';
+import { MatchedPlaceholder, SamplerInterface } from './SamplerInterface';
+import { Strings } from '../../../../../intent-utils/Strings';
 
-export class Sampler {
+export class Sampler implements SamplerInterface {
   private opener: string;
   private closer: string;
+  private cache : {[key: string]: string} = {};
 
   public constructor(opener: string, closer: string) {
     if (!(opener && closer)) {
@@ -19,10 +21,54 @@ export class Sampler {
   }
 
   public placeholder(key: string): string {
-    return null;
+    if (!key) {
+      throw new Error('Invalid key for placeholder');
+    }
+
+    let cached = this.cache[key];
+
+    if (!cached) {
+      if (!((this.opener.indexOf(key) < 0) && (this.closer.indexOf(key) < 0))) {
+        throw new Error('Key for placeholder should not occur in open/close sequences');
+      }
+
+      this.cache[key] = cached = this.opener + key + this.closer;
+    }
+
+    return cached;
   }
 
-  public next(subject: string, from?: number): MatchedPlaceholder {
-    return null;
+  public next(subject: string, from: number = 0): MatchedPlaceholder {
+    if (!subject) {
+      return;
+    }
+
+    let found = undefined;
+    let open = Strings.lookup(subject, from, this.opener);
+
+    while (open >= 0) {
+      let inner = Strings.lookup(subject, open + this.opener.length, this.opener);
+      let close = Strings.lookup(subject, open + this.opener.length, this.closer);
+
+      if (close < 0) {
+        return;
+      }
+
+      if ((inner > 0) && (inner < close)) {
+        found = open;
+        open = inner;
+
+        continue;
+      }
+
+      close += this.closer.length;
+
+      return {
+        open,
+        close,
+        key : subject.substring(open + this.opener.length, close - this.closer.length),
+        next: (found === undefined) ? close : found,
+      };
+    }
   }
 }
--- tests/core/flow/transpiler/SamplerSpec.ts
+++ tests/core/flow/transpiler/SamplerSpec.ts
@@ -18,13 +18,97 @@ describe('Sampler', () => {
   });
 
   describe('placeholder()', () => {
-
     let sample1 = () => [
-      () => ({open: "", close: "", expect: ""}),
+      () => ({open: "{", close: "}", key: "", expect: new Error("Invalid key for placeholder")}),
+      () => ({open: "{", close: "}", key: "{", expect: new Error("Key for placeholder should not occur in open/close sequences")}),
+      () => ({open: "{", close: "}", key: "}", expect: new Error("Key for placeholder should not occur in open/close sequences")}),
     ];
 
-    pit('should return wrapped key', sample1, (data) => {
+    let sample2 = () => [
+      () => ({open: "{", close: "}", key: "a", expect: "{a}"}),
+      () => ({open: "^", close: "$", key: ".", expect: "^.$"}),
+    ];
 
+    pit('should throw on invalid key', sample1, (data) => {
+      let sampler = new Sampler(data.open, data.close);
+
+      expect(() => sampler.placeholder(data.key)).toThrow(data.expect);
+    });
+
+    pit('should return wrapped key', sample2, (data) => {
+      let sampler = new Sampler(data.open, data.close);
+
+      expect(sampler.placeholder(data.key)).toEqual(data.expect);
+    });
+
+  });
+
+  describe('next()', () => {
+    let opener = '{', closer = '}';
+    let sampler: Sampler;
+
+    beforeEach(() => {
+      sampler = new Sampler(opener, closer);
+    });
+
+    let sample1 = () => [
+      () => ({line: null, expect: null}),
+      () => ({line: "non-placeholder", expect: null}),
+      () => ({line: "non-{placeholder", expect: null}),
+      () => ({line: "non-place}holder", expect: null}),
+      () => ({line: "non-}place{holder", expect: null}),
+      () => ({line: "non-}place{holder", expect: null}),
+    ];
+
+    let sample2Gen = (line, key) => {
+      let placeholder = sampler.placeholder(key);
+      let generated = line.replace('$1', placeholder);
+
+      return {
+        line: generated,
+      };
+    };
+
+    let sample3Gen = (line, key) => {
+      let placeholder = sampler.placeholder(key);
+      let generated = line.replace('$1', placeholder);
+
+      return {
+        line: generated,
+        expect: {
+          key  : key,
+          open : line.indexOf('$1'),
+          close: line.indexOf('$1') + placeholder.length,
+        }
+      };
+    };
+
+    let sample2 = () => [
+      () => sample2Gen(`$1`, 'placeholder'),
+      () => sample2Gen(`text $1 text`, 'placeholder'),
+      () => sample2Gen(`$1 text text`, 'placeholder'),
+      () => sample2Gen(`text text $1`, 'placeholder'),
+    ];
+
+    let sample3 = () => [
+      () => sample3Gen(`te${opener}xt $1 text`, 'placeholder'),
+      () => sample3Gen(`text $1 te${opener}xt`, 'placeholder'),
+    ];
+
+    pit('should return void on input without placeholders', sample1, (data) => {
+      expect(sampler.next(data.line)).toBeFalsy();
+    });
+
+    pit('should return match if placeholder is present in line', sample2, (data) => {
+      expect(sampler.next(data.line)).toBeTruthy();
+    });
+
+    pit('should return placeholder and it`s position in line', sample3, (data) => {
+      expect(sampler.next(data.line)).toEqual(jasmine.objectContaining(data.expect));
+    });
+
+    pit('should return innermost placeholder', sample3, (data) => {
+      expect(sampler.next(data.line)).toEqual(jasmine.objectContaining(data.expect));
     });
 
   });
