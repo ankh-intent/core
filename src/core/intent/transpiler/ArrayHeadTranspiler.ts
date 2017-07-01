b5199cc265dcc28fec65cd107cc534d32fe8734a
ankhzet <ankhzet@gmail.com>
2017-07-01 16:16:07 +0300

    Remove obsolete classes

--- src/core/intent/transpiler/ArrayHeadTranspiler.ts
+++
@@ -1,9 +0,0 @@
-
-import { AbstractCompoundTemplate } from '../../flow/transpiler/templates/CompoundTemplate';
-import { Container } from '../../flow/transpiler/Container';
-
-export class ArrayHeadTranspiler extends AbstractCompoundTemplate<Container<any>> {
-  public get code(): string {
-    return `{%.0%}`;
-  }
-}
