ec57dc141a5835ecfbba2b86da0fe7aa30def6fd
ankhzet <ankhzet@gmail.com>
2017-07-02 12:18:48 +0300

    Change returned promise value in file writer

--- src/core/source/FileWriter.ts
+++ src/core/source/FileWriter.ts
@@ -4,13 +4,13 @@ import fs = require('fs');
 import { Source } from './Source';
 
 export class FileWriter {
-  public write(source: Source, encoding: string = 'utf8'): Promise<any> {
+  public write(source: Source, encoding: string = 'utf8'): Promise<Source> {
     return this.assumeDir(path.dirname(source.reference))
       .then(() => new Promise((rs, rj) => {
           fs.writeFile(source.reference, source.content, {encoding: encoding}, function (err) {
             return err
               ? rj(err)
-              : rs();
+              : rs(source);
           });
         })
       )
