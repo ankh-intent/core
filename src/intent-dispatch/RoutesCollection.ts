640c0b8d2101833453c6b7009604a2f154d6609f
ankhzet <ankhzet@gmail.com>
2017-07-08 16:24:33 +0300

    Configure Webpack+React+Typescript for server app

---
+++ src/intent-dispatch/RoutesCollection.ts
@@ -0,0 +1,7 @@
+
+import { ServerOptions } from './ServerOptions';
+
+export class RoutesCollection<O extends ServerOptions> {
+  public configure(app, options: O) {
+  }
+}
---
+++ src/intent-dispatch/application/components/App.tsx
@@ -0,0 +1,22 @@
+
+import * as React from 'react';
+
+export interface AppProps {
+
+}
+
+export interface AppState {
+
+}
+
+export class App extends React.Component<AppProps, AppState> {
+
+  public render() {
+    return (
+      <div>
+        <h3>Hello App!</h3>
+      </div>
+    );
+  }
+
+}
