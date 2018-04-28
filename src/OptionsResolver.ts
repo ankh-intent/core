aef48e64a7d197283f45a164a99e3a1228437c4f
Yegor Shovkun <ankhzet@gmail.com>
2018-04-28 15:07:31 +0300

    Fix: account for Windows-style directory separators

--- src/OptionsResolver.ts
+++ src/OptionsResolver.ts
@@ -8,7 +8,7 @@ export class OptionsResolver {
 
     if (!resolved.resolver.paths.intent) {
       resolved.resolver.paths.intent = path.resolve(
-        path.join(__dirname.replace('/build/', '/'), 'intent-core/intent/specification/lib/')
+        path.join(__dirname.replace(/[\\/]build[\\/]/, '/'), 'intent-core/intent/specification/lib/')
       );
     }
 
