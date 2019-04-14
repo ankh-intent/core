540110da7b5d206aae1bf17aba527875e552ef9b
ankhzet <ankhzet@gmail.com>
2019-04-14 04:37:17 +0300

    fix: dependency on implementation

--- src/core/consumers/ast-compiling/ParseConsumer.ts
+++ src/core/consumers/ast-compiling/ParseConsumer.ts
@@ -1,10 +1,10 @@
+import { TreeNode } from '../../kernel/ast/TreeNode';
 import { TokenVisitor } from '../../kernel/ast/TokenVisitor';
 import { CoreEvent } from '../../kernel/event/CoreEvent';
 import { ParsedEvent } from '../parsing/ParsedEvent';
 import { AbstractConsumer } from '../../kernel/event/consumer/AbstractConsumer';
 import { ConsumerStat } from '../../kernel/event/consumer/ConsumerStat';
 
-import { ChipNode } from '../../../../bin/intent/transpiler/ast/ChipNode';
 import { CoreEventBus } from '../../kernel/event/CoreEventBus';
 import { Source } from '../../kernel/source/Source';
 import { AnalyzedEvent } from './AnalyzedEvent';
@@ -15,10 +15,10 @@ export class AstStat extends ConsumerStat {
   }
 }
 
-export class ParseConsumer extends AbstractConsumer<ParsedEvent, any>{
-  private readonly parser: TokenVisitor<ChipNode>;
+export class ParseConsumer<N extends TreeNode> extends AbstractConsumer<ParsedEvent, any>{
+  private readonly parser: TokenVisitor<N>;
 
-  public constructor(bus: CoreEventBus, parser: TokenVisitor<ChipNode>) {
+  public constructor(bus: CoreEventBus, parser: TokenVisitor<N>) {
     super(bus);
     this.parser = parser;
   }
