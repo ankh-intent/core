b411c1cd3c97f9fe5c55fb6bf361da0c20e4b059
ankhzet <ankhzet@gmail.com>
2019-04-27 14:28:14 +0300

    improve errors log

--- src/core/consumers/errors/ErrorConsumer.ts
+++ src/core/consumers/errors/ErrorConsumer.ts
@@ -9,6 +9,19 @@ import { SyntaxError } from '../../kernel/parser/SyntaxError';
 import { CoreEventBus } from '../../kernel/event/CoreEventBus';
 import { StatEvent } from '../../kernel/event/events/StatEvent';
 
+const NATIVE_ERROR_LOC_MATCHER = /^(?:\s*at\s*)?(.*?)(?:\s*\(([^()]+)\))?$/;
+
+enum RefType {
+  NATIVE,
+  AST,
+}
+
+interface ErrorRef {
+  type: RefType;
+  ref: string;
+  source: string;
+}
+
 export class ErrorConsumer extends AbstractConsumer<ErrorEvent, any>{
   private readonly logger: Logger;
 
@@ -69,60 +82,112 @@ export class ErrorConsumer extends AbstractConsumer<ErrorEvent, any>{
   private fetchSyntaxStack(error: SyntaxError) {
     const hops = [];
     let e: any = error;
-    let last: Error;
 
-    while (e) {
-      if (e instanceof SyntaxError) {
-        hops.unshift(this.describeSyntaxError(e));
-      } else {
-        hops.unshift(e.toString());
+    const consumeErrorDescription = (stack) => {
+      if (stack && stack.length) {
+        hops.unshift(...stack);
+      }
+    };
+
+    while (true) {
+      const isSyntaxError = e instanceof SyntaxError;
+
+      consumeErrorDescription(
+        isSyntaxError
+          ? this.describeSyntaxError(e)
+          : this.describeNativeError(e)
+      );
+
+
+      if (!e.parent) {
+        if (isSyntaxError) {
+          consumeErrorDescription(this.describeNativeError(e));
+        }
+
+        break;
       }
 
-      last = e;
       e = e.parent;
     }
 
     const internal = Strings.getRootSrcPath();
-    const regex = /^(.*?)\s*\(([^()]+)\)$/;
-    const stack = last.stack
-      .split("\n")
-      .concat(hops)
-      .map((line) => line.trim())
-    ;
-    const withoutRefs = stack.map((line) => line.replace(regex, '$1'));
-    console.log(stack, withoutRefs);
-
-    const max = Strings.max(withoutRefs);
+    const stack = this.squashErrors(
+      hops
+        .filter(Boolean)
+        .filter(def => !(def.source && def.source.startsWith(internal)))
+    );
+    const max = Strings.max(stack.map(def => def.ref));
 
     const lines = stack
-      .map((line) => {
-        const [, ref, loc] = line.match(regex) || [,,,];
-
-        if (loc && loc.startsWith(internal)) {
-          return '';
-        }
-
-        return `\t${Strings.pad(ref, max, ' ')} (${loc})`;
+      .map((def) => {
+        return `\tat ${Strings.pad(def.ref, max, ' ')} (${def.source})`;
       })
       .filter(Boolean)
     ;
 
-    return [last.message].concat(lines);
+    return [e.message].concat(lines);
   }
 
-  private describeSyntaxError(error: SyntaxError) {
+  private describeSyntaxError(error: SyntaxError): ErrorRef[]|undefined {
     let source;
 
     if (error.source) {
       const loc = error.source.location(error.pos);
-      source = ` (${error.source.reference}:${loc.line}:${loc.column})`;
+      source = `${error.source.reference}:${loc.line}:${loc.column}`;
     } else {
       source = '';
     }
 
     const match = error.message.match(/Failed @(.+)$/);
-    const method = match ? match[1] : 'unknown';
 
-    return `at AST::${method}${source}`;
+    if (match) {
+      return [{
+        type: RefType.AST,
+        ref: match[1],
+        source,
+      }];
+    }
+  }
+
+  private describeNativeError(error: Error): ErrorRef[] {
+    return error.stack
+      .split("\n")
+      .map(line => line.trim())
+      .filter(Boolean)
+      .map((line) => {
+        const [, ref, source] = line.match(NATIVE_ERROR_LOC_MATCHER) || [,,,];
+
+        return {
+          type: RefType.NATIVE,
+          ref,
+          source,
+        };
+      })
+    ;
+  }
+
+  private squashErrors(descriptors: ErrorRef[]): ErrorRef[] {
+    return descriptors
+      .reduce((acc: ErrorRef[], current: ErrorRef) => {
+        if (current.type === RefType.AST) {
+          const prev = acc[acc.length - 1];
+
+          if (prev && (prev.type === RefType.AST) && (prev.source === current.source)) {
+            prev.ref = `${current.ref}->${prev.ref}`;
+          } else {
+            acc.push(current);
+          }
+        } else {
+          acc.push(current);
+        }
+
+        return acc;
+      }, [] as any)
+      .map(def => (
+        (def.type === RefType.AST)
+          ? { ...def, ref: `AST::${def.ref}` }
+          : def
+      ))
+    ;
   }
 }
