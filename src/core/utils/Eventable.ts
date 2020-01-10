f311af09ae5dc55dfb8c1e8244d8bcf9698bf3be
ankhzet <ankhzet@gmail.com>
2020-01-10 14:19:29 +0200

    tweak: optimize event emitter memory footprint

--- src/core/utils/Eventable.ts
+++ src/core/utils/Eventable.ts
@@ -1,7 +1,7 @@
 
 export class Eventable {
 	private static _e_uid: number = 0;
-	private _e_listeners: {[event: string]: {[index: number]: Function}};
+	private _e_listeners?: {[event: string]: {[index: number]: Function}};
 
 	on(event: string, handler: Function): number {
 		if (!this._e_listeners) {
@@ -27,38 +27,36 @@ export class Eventable {
 
 	off(event?: string, uid?: number) {
 		if (!event) {
-			this._e_listeners = null;
+			this._e_listeners = undefined;
 
 			return;
 		}
 
-    const listeners = this._e_listeners[event];
+		if (this._e_listeners) {
+      const listeners = this._e_listeners[event];
 
-		if (listeners) {
-			if (!uid) {
-				delete this._e_listeners[event]
-			} else {
-				delete listeners[uid];
-			}
-		}
+      if (listeners) {
+        if (!uid) {
+          delete this._e_listeners[event]
+        } else {
+          delete listeners[uid];
+        }
+      }
+    }
 	}
 
 	emit(event: string, ...payload: any[]): number {
 		let handled = 0;
 
-		if (this._e_listeners) {
-      const listeners = this._e_listeners[event];
+    const listeners = this._e_listeners && this._e_listeners[event];
 
-			if (listeners) {
-        const uids = Object.keys(listeners);
+    if (listeners) {
+      for (const uid of Object.keys(listeners)) {
+        listeners[uid](...payload, event);
 
-				for (const uid of uids) {
-					listeners[uid](...payload, event);
-
-					handled++;
-				}
-			}
-		}
+        handled++;
+      }
+    }
 
 		return handled;
 	}
