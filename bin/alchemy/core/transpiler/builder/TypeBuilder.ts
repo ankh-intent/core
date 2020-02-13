ffc3bea9ca5fe2c95272d65b9ad2eeddb71910d0
ankhzet <ankhzet@gmail.com>
2020-02-13 12:25:44 +0200

    feat: support array type shortcut

--- bin/alchemy/core/transpiler/builder/TypeBuilder.ts
+++ bin/alchemy/core/transpiler/builder/TypeBuilder.ts
@@ -15,15 +15,22 @@ export class TypeBuilder extends BaseBuilder<TypeNode, TypeChildren> {
   protected build(tokens, { get, ensure }: TypedTokenMatcherInterface) {
     const qualifier = this.child.qualifier(tokens);
     let generic: TypeGenericNode<TypeNode>|null = null;
+    let isArray = false;
 
     if (get.symbol('<')) {
       generic = this.child.type_generic(tokens);
       ensure.symbol('>');
     }
 
+    if (get.symbol('[')) {
+      isArray = true;
+      ensure.symbol(']');
+    }
+
     return new TypeNode(
       qualifier,
       generic,
+      isArray,
     );
   }
 }
