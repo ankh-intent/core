24f8ef6d36456b59aac70fbc6d29333153faa561
ankhzet <ankhzet@gmail.com>
2020-02-13 11:09:11 +0200

    fix: 'alchemy' uses not resolved

--- bin/alchemy/core/chips/use/LibraryUseResolver.ts
+++ bin/alchemy/core/chips/use/LibraryUseResolver.ts
@@ -13,11 +13,11 @@ export class LibraryUseResolver implements UseResolverInterface {
   }
 
   supports(from: Module, identifier: QualifierNode): boolean {
-    return identifier.name === 'Intent';
+    return identifier.name === 'Alchemy';
   }
 
   public resolve(from: Module, identifier: QualifierNode): Module {
-    const relative = identifier.path('/').toLowerCase().replace(/^intent[\/\\]/, '') + '.alc';
+    const relative = identifier.path('/').toLowerCase().replace(/^alchemy[\/\\]/, '') + '.alc';
     const resolved = path.join(this.config.internal, relative);
 
     return new Module(resolved);
